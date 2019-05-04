2fc0532fe53cb597987eae1214fd700f1b572f52
Haeseong Jeon <seong0428@gmail.com>
2019-05-04 21:02:08 +0900

    Initial commit

---
+++ src/main/java/com/depromeet/deprocheck/deprocheckapi/DeprocheckApiApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.deprocheck.deprocheckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeprocheckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeprocheckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depromeet/deprocheck/deprocheckapi/DeprocheckApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.depromeet.deprocheck.deprocheckapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeprocheckApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
