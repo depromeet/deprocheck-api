6d04577fbb71705dea490d517da3afe17b2004d2
Haeseong Jeon <seong0428@gmail.com>
2019-05-04 21:07:47 +0900

    Add swagger

---
+++ src/main/java/com/depromeet/deprocheck/deprocheckapi/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.depromeet.deprocheck.deprocheckapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+}
