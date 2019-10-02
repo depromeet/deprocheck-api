ceb19bfb354f3f66ad4b4d87fa519f6131f31b30
전해성 <seong0428@gmail.com>
2019-10-03 02:14:33 +0900

    test #21 - 테스트를 추가합니다

--- src/test/java/com/depromeet/deprocheck/deprocheckapi/helper/AdminControllerApi.java
+++ src/test/java/com/depromeet/deprocheck/deprocheckapi/helper/AdminControllerApi.java
@@ -1,6 +1,7 @@
 package com.depromeet.deprocheck.deprocheckapi.helper;
 
 import com.depromeet.deprocheck.deprocheckapi.ui.dto.MemberCreateRequest;
+import com.depromeet.deprocheck.deprocheckapi.ui.dto.SessionCreateRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,6 +28,16 @@ public final class AdminControllerApi {
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(objectMapper.writeValueAsString(memberCreateRequest)))
+                .andDo(print())
+                .andReturn();
+        return TestApiResultHelper.of(mvcResult, objectMapper);
+    }
+
+    public TestApiResultHelper createSession(String accessToken, SessionCreateRequest sessionCreateRequest) throws Exception {
+        MvcResult mvcResult = mockMvc.perform(post("/api/admin/sessions")
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(sessionCreateRequest)))
                 .andDo(print())
                 .andReturn();
         return TestApiResultHelper.of(mvcResult, objectMapper);
